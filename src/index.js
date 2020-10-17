c382c00af43b6b7309bcc21dfa1024eed51bb081
Natallia22 <zhuravleva.977@yandex.by>
2020-10-17 15:28:27 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  testArray = [];
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  for (let i = 0; i < str.length; i++) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+          if (
+              (bracketsConfig[j][0] === bracketsConfig[j][1]) &&
+              (str[i] === bracketsConfig[j][1])
+          ) {
+              if (testArray[testArray.length - 1] === str[i]) {
+                  testArray.pop();
+              } else {
+                  testArray.push(str[i]);
+              }
+          } else {
+              if (str[i] === bracketsConfig[j][0]) {
+                  testArray.push(str[i]);
+              }
+              if (
+                  (str[i] === bracketsConfig[j][1]) &&
+                  (testArray[testArray.length - 1] === bracketsConfig[j][0])
+              ) {
+                  testArray.pop();
+              }
+          }
+      }
+  }
+  if (testArray.length === 0) {
+      return true;
+  } else {
+      return false;
+  }
+};
+
